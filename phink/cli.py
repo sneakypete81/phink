6e4067e0a03fc8f180499c6440cd6ab781a13bf7
sneakypete81 <sneakypete81@gmail.com>
2019-11-28 20:39:18 +0000

    Add serve command with livereload

--- phink/cli.py
+++ phink/cli.py
@@ -1,8 +1,9 @@
 import sys
 from typing import List
 
-from .build import build
 from .init import init
+from .build import build
+from .serve import serve
 
 
 def main(args: List[str] = sys.argv[1:]) -> None:
@@ -10,10 +11,12 @@ def main(args: List[str] = sys.argv[1:]) -> None:
         args = ["build"]
 
     command = args[0]
-    if command == "build":
-        build()
-    elif command == "init":
+    if command == "init":
         init()
+    elif command == "build":
+        build()
+    elif command == "serve":
+        serve()
     else:
         print(f"Invalid command: {command}")
         sys.exit(1)
---
+++ phink/serve.py
@@ -0,0 +1,15 @@
+from livereload import Server
+
+from .build import build, BUILD_PATH
+
+
+def serve() -> None:
+    build()
+
+    server = Server()
+    server.watch("**/*", build, ignore=check_ignore)
+    server.serve(root=BUILD_PATH)
+
+
+def check_ignore(path):
+    return path.startswith(str(BUILD_PATH))
