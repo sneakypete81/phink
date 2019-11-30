170b85c60e292fad2d701ce6ac6da932a480e10d
sneakypete81 <sneakypete81@gmail.com>
2019-11-30 18:49:12 +0000

    Add project dir to path when running Sphinx

--- phink/templates/conf.py
+++ phink/templates/conf.py
@@ -4,6 +4,16 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath(".."))
+
+# -- General configuration ------------------------------------------------
+
 project = "{data.project}"
 
 extensions = ["sphinx.ext.napoleon", "recommonmark"]
@@ -11,6 +21,8 @@ extensions = ["sphinx.ext.napoleon", "recommonmark"]
 master_doc = "index"
 source_suffix = [".rst", ".md"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output ----------------------------------------------
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
